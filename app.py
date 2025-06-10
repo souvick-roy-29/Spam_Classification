f36171f09b8bb4f080e78059155d035569be3701
souvick-roy-29 <souvickroy2354@gmail.com>
2025-06-10 13:30:10 +0530

    Updatation

--- app.py
+++ app.py
@@ -5,8 +5,15 @@ import nltk
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 
-nltk.download('punkt')
-nltk.download('stopwords')
+try:
+    nltk.data.find('tokenizers/punkt')
+except LookupError:
+    nltk.download('punkt')
+
+try:
+    nltk.data.find('corpora/stopwords')
+except LookupError:
+    nltk.download('stopwords')
 
 ps = PorterStemmer()
 stop_words = set(stopwords.words('english'))
